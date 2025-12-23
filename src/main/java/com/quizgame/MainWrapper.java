3920c7f7e0e86750377a925f61b03d2f8ca0bc7e
MainDjibson <jiby.njaay@gmail.com>
2025-12-23 01:14:42 +0100

    Ajout de la classe MainWrapper

---
+++ src/main/java/com/quizgame/MainWrapper.java
@@ -0,0 +1,8 @@
+package com.quizgame;
+
+public class MainWrapper {
+    public static void main(String[] args) {
+        // Cette ligne appelle ton application JavaFX
+        QuizGameApp.main(args);
+    }
+}
